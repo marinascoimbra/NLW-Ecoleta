cb42559879ba132ec72db8176fbcc34d0d5713a0
Marina <marinascoimbra@hotmail.com>
2020-06-07 14:36:23 -0300

    FIT: aula 03

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -9,11 +9,14 @@ document
         let indexOfSelectedState = event.target.selectedIndex;
         stateInput.value = event.target.options[indexOfSelectedState].text;
 
+        citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+        citySelect.disabled = true
+
         fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufSelected}/municipios`)
         .then(res => res.json())
         .then(data => {
             for (city of data) {
-                citySelect.innerHTML += `<option value=${city.id}>${city.nome}</option>`
+                citySelect.innerHTML += `<option value=${city.nome}>${city.nome}</option>`
             }
 
             citySelect.disabled = false
@@ -35,3 +38,30 @@ function populateUFs() {
 
 populateUFs()
 
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+const collectedItems = document.querySelector("input[name=items]");
+
+for (let item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+let selectedItens = [];
+
+function handleSelectedItem(event) {
+    const itemLi = event.target;
+
+    itemLi.classList.toggle("selected");
+    const itemId = itemLi.dataset.id;
+
+    const alreadySelected = selectedItens.findIndex(item => item == itemId)
+
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItens.filter(item => item != itemId)
+        selectedItens = filteredItems;
+    } else {
+        selectedItens.push(itemId);
+    }
+
+    collectedItems.value = selectedItens;
+}
