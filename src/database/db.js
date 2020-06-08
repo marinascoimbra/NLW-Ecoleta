3000efde9db5a91190249ce6b84604423e2e2409
Marina <marinascoimbra@hotmail.com>
2020-06-07 22:21:56 -0300

    PARTIAL: aula 05

---
+++ src/database/db.js
@@ -0,0 +1,57 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./src/database/database.db");
+
+module.exports = db;
+
+// db.serialize(() => {
+//     db.run(`
+//         CREATE TABLE IF NOT EXISTS places(
+//             id INTEGER PRIMARY KEY AUTOINCREMENT,
+//             image TEXT,
+//             name TEXT,
+//             address TEXT,
+//             address2 TEXT,
+//             state TEXT,
+//             city TEXT,
+//             items TEXT
+//         );
+//     `)
+
+//     const query = `
+//     INSERT INTO places (
+//         image,
+//         name,
+//         address,
+//         address2,
+//         state,
+//         city,
+//         items
+//     ) VALUES (?,?,?,?,?,?,?);
+// `
+//     db.run(query, [
+//         "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1101&q=80", 
+//         "Papersider",
+//         "Guilherme Gemballa, Jardim América",
+//         "Nº 260",
+//         "Santa Catarina",
+//         "Rio do Sul",
+//         "Papéis e Papelão"
+//     ], function(error) {
+//         if (error) {
+//             return console.log(error);
+//         }
+
+//         console.log("Cadastrado com Sucesso");
+//         console.log(this);
+//     });
+
+//     db.all(`SELECT * FROM places`, function(error, rows) {
+//         if (error) {
+//             return console.log(error);
+//         }
+
+//         console.log("Aqui estão seus registros: ");
+//         console.log(rows);
+//     })
+// })
--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const server = express();
 
+const db = require("./database/db");
+
 server.use(express.static("public"));
+
+server.use(express.urlencoded({ extended: true }));
 
 const nunjucks = require("nunjucks");
 nunjucks.configure("src/views", {
     express: server,
     noCache: true
 });
-
-
 
 server.get("/", (req, res) => {
     return res.render("index.html")
@@ -19,8 +21,49 @@ server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
 });
 
+server.post("/save-point", (req, res) => {
+    const query = `
+    INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+    ) VALUES (?,?,?,?,?,?,?);`
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+    
+    db.run(query, values, function(error) {
+        if (error) {
+            return console.log(error);
+        }
+
+        return res.render("create-point.html", { saved: true });
+    });
+})
+
 server.get("/search-results", (req, res) => {
-    return res.render("search-results.html")
+
+    db.all(`SELECT * FROM places`, function(error, rows) {
+        if (error) {
+            return console.log(error);
+        }
+
+        const total = rows.length;
+
+        return res.render("search-results.html", { places: rows, total })
+    });
+
 });
 
 server.listen(3000);
